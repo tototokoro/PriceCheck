8ac8190a5ad98812ea7165b84007cb869c3e641f
Yuhi Tokoro <tokoroyuhi@gmail.com>
2018-08-04 20:07:27 +0900

    バーコード読み取り実装

---
+++ PriceCheck/CameraViewController.swift
@@ -0,0 +1,88 @@
+import Foundation
+import AVFoundation
+import UIKit
+
+class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    // カメラやマイクの入出力を管理するオブジェクトを生成
+    private let session = AVCaptureSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // カメラやマイクのデバイスそのものを管理するオブジェクトを生成（ここではワイドアングルカメラ・ビデオ・背面カメラを指定）
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],mediaType: .video,position: .back)
+        
+        // ワイドアングルカメラ・ビデオ・背面カメラに該当するデバイスを取得
+        let devices = discoverySession.devices
+        
+        //　該当するデバイスのうち最初に取得したものを利用する
+        if let backCamera = devices.first {
+            do {
+                // QRコードの読み取りに背面カメラの映像を利用するための設定
+                let deviceInput = try AVCaptureDeviceInput(device: backCamera)
+                
+                if self.session.canAddInput(deviceInput) {
+                    self.session.addInput(deviceInput)
+                    
+                    // 背面カメラの映像からコードを検出するための設定
+                    let metadataOutput = AVCaptureMetadataOutput()
+                    
+                    if self.session.canAddOutput(metadataOutput) {
+                        self.session.addOutput(metadataOutput)
+                        
+                        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                        metadataOutput.metadataObjectTypes = [.ean13]
+                        
+                        // 背面カメラの映像を画面に表示するためのレイヤーを生成
+                        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                        previewLayer.frame = self.view.bounds
+                        previewLayer.videoGravity = .resizeAspectFill
+                        self.view.layer.addSublayer(previewLayer)
+                        
+                        // 読み取り開始
+                        self.session.startRunning()
+                    }
+                }
+            } catch {
+                print("Error occured while creating video device input: \(error)")
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.session.startRunning()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        for data in metadataObjects as! [AVMetadataMachineReadableCodeObject]{
+            //バーコードかどうか確認
+            if data.type != .ean13 {continue}
+            
+            //内容が空かどうかの確認
+            if data.stringValue == nil {continue}
+            
+            if (data.stringValue?.prefix(3).contains("192"))! {
+                print("下のバーコードを読み取った\n修正しようね")
+                continue
+            }
+            
+            //読み取り終了
+            self.session.stopRunning()
+            
+            performSegue(withIdentifier: "showResultView", sender: data.stringValue)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showResultView" {
+            let nextViewController = segue.destination as! ResultViewController
+            nextViewController.codeNum = sender as? String
+        }
+    }
+}
--- PriceCheck/ResultViewController.swift
+++ PriceCheck/ResultViewController.swift
@@ -1,18 +1,20 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ResultViewController: UIViewController {
+    var codeNum: String?
+    
+    @IBOutlet weak var number: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        number.text = codeNum
         
+        print("sss\(number)")
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func toCameraView(_ sender: Any) {
-        performSegue(withIdentifier: "showCameraView", sender: nil)
     }
 }
